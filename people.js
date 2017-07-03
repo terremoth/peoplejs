00eae5f11a9f446e6141dca05d4a9b104f92eecf
terremoth <dutr4@outlook.com>
2017-07-03 01:54:03 -0300

    Add clear all cookies function

--- people.js
+++ people.js
@@ -304,6 +304,17 @@ function listCookies() {
     return cookies;
 }
 
+function clearCookies() { // in local path
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
 function windowSize() {
     var w = window,
         d = document,
