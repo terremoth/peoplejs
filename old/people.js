b53f1e579cfdcf2912996a938d8b682ff62b3365
Lucas M. Dutra <terremoth@users.noreply.github.com>
2018-05-14 01:03:14 -0300

    add rot13 function

--- old/people.js
+++ old/people.js
@@ -1299,6 +1299,14 @@ function baseConverter(strTxt, from, to, separator) {
     
 } 
 
+function rot13(str) {
+  var input     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  var output    = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+  var index     = x => input.indexOf(x);
+  var translate = x => index(x) > -1 ? output[index(x)] : x;
+  return str.split('').map(translate).join('');
+}
+
 String.prototype.changeToBase = function(strTxt, from, to, separator) {
     return baseConverter(strTxt, from, to, separator);
 };
